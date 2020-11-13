28be3a5a7e95d9323780bd4b07187bd59318e062
Ariescat <1275563227@qq.com>
2020-11-13 12:25:29 +0800

    build: 模块重构

---
+++ game-test/src/main/java/com/mmorpg/mir/GameStart.java
@@ -0,0 +1,14 @@
+package com.mmorpg.mir;
+
+/**
+ * 游戏启动器
+ *
+ * @author Ariescat
+ * @version 2020/11/13 12:15
+ */
+public class GameStart {
+
+	public static void main(String[] args) {
+		System.out.println("Game Start...");
+	}
+}
